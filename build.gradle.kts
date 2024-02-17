c4fe485da53fadebf2936d71c8944fdb64280302
leegod <lee20040919@gmail.com>
2024-02-17 13:55:40 +0800

    feat: Bumped to fairy 0.7.0b3-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,19 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     //Java plugin
-    id("java")
+    id("java-library")
+
+    //Fairy framework plugin
+    id("io.fairyproject") version "0.7.0b3-SNAPSHOT"
+
+    // Dependency management plugin
+    id("io.spring.dependency-management") version "1.1.0"
 
     //Kotlin plugin
-    // id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
 
     //Shadow plugin, provides the ability to shade fairy and other dependencies to compiled jar
     id("com.github.johnrengelman.shadow") version "7.1.2"
-
-    //Fairy framework plugin
-    id("io.fairyproject") version "1.3.0b2"
 }
 
 group = properties("group")
@@ -21,38 +24,56 @@ version = properties("version")
 
 // Fairy configuration
 fairy {
-    //Fairy version
-    version.set(properties("fairy.version"))
-    //Main Package
+    name.set(properties("name"))
+    // Main Package
     mainPackage.set(properties("package"))
-    //Install bukkit platform to this project.
-    platform("bukkit")
+    // Fairy Package
+    fairyPackage.set(properties("package") + ".fairy")
 
-    //Should fairy framework be compiled for runtime (by default it's compileOnly)
-    doCompile()
-    //Should bootstrap be installed for this project
-    doBootstraps()
-    //Should test tool kits be installed for this project
-    // doTests()
+    bukkitProperties().bukkitApi = "1.13"
+}
 
-    //Modules
-    // module("command")
+runServer {
+    version.set(properties("spigot.version"))
+}
+
+dependencies {
+    api("io.fairyproject:bukkit-bundles")
+    api("io.fairyproject:module.animation")
+    api("io.fairyproject:bukkit-command")
+    api("io.fairyproject:bukkit-menu")
+    api("io.fairyproject:module.hologram")
+    api("io.fairyproject:module.config")
+    api("io.fairyproject:bukkit-xseries")
+    api("io.fairyproject:bukkit-items")
+    api("io.fairyproject:module.nametag")
+    api("io.fairyproject:module.sidebar")
+    api("io.fairyproject:bukkit-visibility")
+    api("io.fairyproject:bukkit-visual")
+    api("io.fairyproject:bukkit-timer")
+    api("io.fairyproject:bukkit-nbt")
+    api("io.fairyproject:module.tablist")
 }
 
 // Repositories
 repositories {
     mavenCentral()
+    maven(url = uri("https://oss.sonatype.org/content/repositories/snapshots/"))
+    maven(url = uri("https://repo.codemc.io/repository/maven-public/"))
     // Spigot's repository for spigot api dependency
     maven(url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/"))
+    maven(url = uri("https://repo.imanity.dev/imanity-libraries"))
 }
 
 // Dependencies
 dependencies {
     // Spigot dependency
-    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:${properties("spigot.version")}-R0.1-SNAPSHOT")
 }
 
 tasks.withType(ShadowJar::class.java) {
     // Relocate fairy to avoid plugin conflict
     relocate("io.fairyproject", "${properties("package")}.fairy")
+    relocate("net.kyori", "${properties("package")}.fairy.libs.kyori")
+    relocate("com.cryptomorin.xseries", "${properties("package")}.fairy.libs.xseries")
 }
--- src/main/java/org/example/template/BukkitTemplatePlugin.java
+++ src/main/java/org/example/template/BukkitTemplatePlugin.java
@@ -1,8 +1,10 @@
 package org.example.template;
 
+import io.fairyproject.FairyLaunch;
 import io.fairyproject.log.Log;
 import io.fairyproject.plugin.Plugin;
 
+@FairyLaunch
 public class BukkitTemplatePlugin extends Plugin {
 
     @Override
